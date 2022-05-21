f2fd774642b602c1f16acc82fc51911d740f77a9
SableVector <lutchenkoroma@gmail.com>
2022-05-21 12:42:38 +0300

    added homework of destructorization

--- destructorization.js
+++ destructorization.js
@@ -41,7 +41,54 @@ console.log(name, info);
 
 // =>
 
-function logPerson({name,age}) {
+function logPerson({ name, age }) {
     console.log(name + ' ' + age);
 }
 logPerson(person);
+
+// =================================
+// 1 Используя rest оператор и деструктуризацию, создать функцию, которая принимает любое количество аргументов и возвращает объект, содержащий первый аргумент и массив из остатка:
+
+// func(‘a’, ‘b’, ‘c’, ‘d’) → 
+// {
+//   first: ‘a’,
+//   other: [‘b’, ‘c’, ‘d’]
+// }
+
+function foo(firstElement, ...arg) {
+    return {
+        first: firstElement,
+        other: arg
+    }
+}
+
+console.log(foo('a', 'b', 'c', 'd'));
+
+// 2. Организовать функцию getInfo, которая принимает объект вида
+// { name: ...,  info: { employees: [...], partners: [ … ]  } }
+// и выводит в консоль имя (если имени нет, показывать ‘Unknown’) и первые две компании из массива partners:
+
+// const organisation = {  
+//   name: 'Google',   
+//   info: { employees: [‘Vlad’, ‘Olga’], partners: ['Microsoft', 'Facebook', 'Xing']   } 
+// };
+// getInfo(organisation); → 
+// Name: Google 
+// Partners: Microsoft Facebook
+
+const obj2 = {
+    name: 'Google',
+    info: {
+        employees: ['‘Vlad’', '‘Olga’'],
+        partners: ['Microsoft', 'Facebook', 'Xing']
+    }
+}
+
+function getInfo({ name, info: { partners: [first, second] } }) {
+    console.log(`Name: ${name}`, `Partners: ${first}, ${second}`);
+}
+
+getInfo(obj2);
+
+
+
